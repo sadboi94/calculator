6a306e7e880d1f0ee8c4ab5730d1202585adefbb
sadboi94 <hegyaljai.aadam@gmail.com>
2022-01-28 10:32:01 +0100

    Add files via upload

--- backend/source/config/config.ts
+++ backend/source/config/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const SERVER_HOST = process.env.SERVER_HOST;
+const SERVER_HOST = process.env.SERVER_HOST || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 1337;
 
 const SERVER = {
